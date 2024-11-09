e9539e226d8c87f87202b56adbc6afc9ce97fa87
chi-wei-lien <wilsonwilson3124@gmail.com>
2024-11-09 17:59:53 -0500

    add docker and heroku

--- backend/build.sh
+++ backend/build.sh
@@ -4,21 +4,34 @@ clear() {
 }
 
 migrate() {
-  python manage.py makemigrations
-  python manage.py migrate
+  poetry run python manage.py makemigrations
+  poetry run python manage.py migrate
 }
 
 start() {
-  python manage.py runserver
+  poetry run python manage.py runserver
 }
 
 create_super_user() {
-  python manage.py createsuperuser
+  poetry run python manage.py createsuperuser
 }
 
 db_init() {
-  python manage.py insert_init_languages
+  poetry run python manage.py insert_init_languages
 }
+
+gen_req() {
+  poetry export --without-hashes --format=requirements.txt > requirements.txt
+}
+
+docker_build() {
+  gen_req
+  docker stop grind-hub-backend
+  docker rm grind-hub-backend
+  docker build -t web:latest .
+  docker run -d --name grind-hub-backend -e "PORT=8765" -e "DEBUG=1" -p 8007:8765 web:latest
+}
+
 
 case "$1" in
   clear)
@@ -36,7 +49,13 @@ case "$1" in
   db_init)
     db_init
     ;;
+  docker_build)
+    docker_build
+    ;;
+  gen_req)
+    gen_req
+    ;;
   *)
-    echo "Usage: $0 {clear|migrate|start|create_super_user|db_init}"
+    echo "Usage: $0 {clear|migrate|start|create_super_user|db_init|docker_build|gen_req}"
     exit 1
 esac
--- readme.md
+++ readme.md
@@ -3,7 +3,8 @@
 - User (<ins>id, username</ins>, first_name, last_name, password)
 - Language (<ins>name</ins>) <br>
 - Question (<ins>q_id</ins>, name, link, posted_by, posted_time)
-- MarkQuestion (<ins>user_id, q_id</ins>, difficulty, done) <br>
+  - posted_by is the foreign key to User's id
+- MarkQuestion (<ins>user_id, q_id</ins>, done) <br>
   - user_id is the foreign key to User's id
   - q_id is the foreign key to Question's q_id
 - Solution (<ins>s_id</ins>, name, q_id, lang_name, posted_by, code, notes, posted_time, tc, sc)
