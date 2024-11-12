6d2c05d115250951bb78238081e463108c453a98
chi-wei-lien <wilsonwilson3124@gmail.com>
2024-11-12 16:46:46 -0500

    add privacy policy

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,12 +1,27 @@
 const Footer = () => {
   return (
     <div className="flex items-center justify-center bg-red-400 h-60">
-      <span className="text-sm text-white">Made by&nbsp;</span>
-      <span className="text-2xl text-white font-monofett">
-        <a href="https://www.chiweilien.com/" target="_blank">
-          Willy Lien
-        </a>
-      </span>
+      <div>
+        <div className="flex items-center justify-center">
+          <span className="text-sm text-white">Made by&nbsp;</span>
+          <span className="text-2xl text-white font-monofett">
+            <a href="https://www.chiweilien.com/" target="_blank">
+              Willy Lien
+            </a>
+          </span>
+        </div>
+        <div className="flex items-center justify-center">
+          <span className="text-sm text-white">
+            <a
+              href="https://www.freeprivacypolicy.com/live/cf02c831-a6b3-46ea-abbc-c38d77db3f76
+"
+              target="_blank"
+            >
+              Privacy Policy
+            </a>
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
