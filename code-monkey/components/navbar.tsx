787861f7d3c83447141186d893e295f4b474a25b
chi-wei-lien <wilsonwilson3124@gmail.com>
2024-12-19 22:32:47 -0500

    add navbar

--- code-monkey/components/navbar.tsx
+++ code-monkey/components/navbar.tsx
@@ -1,10 +1,28 @@
 import { pacifico } from "@/app/fonts";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="absolute pl-8 pt-3 w-screen">
-      <div className={`${pacifico.className} text-2xl text-fontLogo`}>
-        🐵 code monkey
+    <div className="w-screen relative">
+      <div className="absolute pl-8 pt-3">
+        <div className="flex items-center gap-7">
+          <Link
+            href="/"
+            className={`${pacifico.className} text-2xl text-fontLogo`}
+          >
+            🐵 code monkey
+          </Link>
+          <div className="pt-2">
+            <Link href="/algo-guide" className="text-fontLogo font-bold">
+              Algo Guide
+            </Link>
+          </div>
+          <div className="pt-2">
+            <Link href="/" className="text-fontLogo font-bold">
+              LeetCode Colab
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
