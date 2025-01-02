1c331c648c124e75dccc6f0fa51c21316fc9e326
chi-wei-lien <wilsonwilson3124@gmail.com>
2025-01-02 10:32:23 +0800

    fix colab group redirecting

--- code-monkey/app/leetcode-colab/page.tsx
+++ code-monkey/app/leetcode-colab/page.tsx
@@ -1,20 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
-import { getGroups } from "@/lib/api/group/getGroups";
-import { redirect } from "next/navigation";
-
 const DefaultPage = () => {
-  useEffect(() => {
-    const redirectGroup = async () => {
-      const groups = await getGroups();
-      if (groups) {
-        redirect(`/leetcode-colab/${groups[0].group_id}`);
-      }
-    };
-
-    redirectGroup();
-  }, []);
   return <div>default page</div>;
 };
 
