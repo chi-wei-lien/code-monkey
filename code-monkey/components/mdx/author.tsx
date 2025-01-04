ba88cfc9acfa2b63637eabb6693471c22565387d
chi-wei-lien <wilsonwilson3124@gmail.com>
2025-01-04 21:07:14 +0800

    add bfs

--- code-monkey/components/mdx/author.tsx
+++ code-monkey/components/mdx/author.tsx
@@ -9,8 +9,8 @@ const Author: React.FC<AuthorProps> = ({ children }) => {
   return (
     <div className="rounded-md border-2 border-dashed border-themeBrown bg-yellow-200 p-4">
       <div className="font-bold">About the Author</div>
-      <div className="flex items-center justify-between gap-4">
-        <div className="w-96">
+      <div className="flex flex-col items-center justify-center lg:flex-row lg:justify-between lg:gap-4">
+        <div className="flex w-96 justify-center">
           <Image
             className="rounded-lg"
             src="/images/me.jpg"
